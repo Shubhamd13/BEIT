4f89e79e2badf82a8b78d28efff15ba8edac6c24
Shubhamd13 <30868812+Shubhamd13@users.noreply.github.com>
2019-10-21 20:52:11 +0530

    svmtypes

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # BEIT
+Required s/w
+- Python2.7
+- Java 1.8
+- R 3.6
+- Rstudio
+- Weka 3-8
+- GraphViz 2
+- NetBeans
+
+# R packages
+- Arules
+- ArulesViz
+
+# Python packages
+- sklearn
+- keras
+- pandas
+- numpy
+- matplotlib
+- mlxtend
+- ann_visualizer
+- tensorflow
--- prac5.py
+++ prac5.py
@@ -4,13 +4,7 @@ from matplotlib import style
 style.use("ggplot")
 from sklearn import svm
 from mlxtend.plotting import plot_decision_regions
-
-x = [1, 5, 1.5, 8, 1, 9]
-y = [2, 8, 1.8, 8, 0.6, 11]
-print(x,y)
-plt.scatter(x,y)
-plt.show()
-# feature list in capital X, x is a feature and y is a feature
+#linear
 X = np.array([[1,2],
              [5,8],
              [1.5,1.8],
@@ -19,15 +13,17 @@ X = np.array([[1,2],
              [9,11]])
 #labels
 y = np.array([0,1,0,1,0,1])
+#watch plot
+plt.scatter(X[:,0],X[:,1],c=y)
+plt.show()
 #define classfier with C = 1
 clf = svm.SVC(kernel='linear', C = 1.0)
 clf.fit(X,y)
-w = clf.coef_[0]
-print(w)
 plot_decision_regions(X=X, 
                       y=y,
                       clf=clf, 
                       legend=1)
+plt.show()
 print("Prediction of target for 0.58,0.76 values:")
 t= clf.predict([[0.58,0.76]])
 print(t)
@@ -35,3 +31,43 @@ print(t)
 print("Prediction of target for 10.58 ,10.76 value")
 t= clf.predict([[10.58,10.76]])
 print(t)
+
+#rbf
+X = np.array([[2,4],
+             [2.3,4.2],
+             [3,9],
+             [3.3,9.3],
+             [1,0.6],
+             [1.2,0.7],
+              [0,0],
+              [4,16]])
+#labels
+y = np.array([0,1,0,1,0,1,0,1])
+#watch plot
+plt.scatter(X[:,0],X[:,1],c=y)
+plt.show()
+clf = svm.SVC(kernel='rbf',gamma="auto")
+clf.fit(X,y)
+plot_decision_regions(X=X, 
+                      y=y,
+                      clf=clf, 
+                      legend=1)
+plt.show()
+#poly
+#rbf
+X = np.array([[2,4],
+             [2.3,4.2],
+             [3,9],
+             [3.3,9.3],
+             [1,0.6],
+             [1.4,0.7],
+              [0,0],
+              [4,16]])
+#labels
+y = np.array([0,1,0,1,0,1,0,1])
+poly_svc = svm.SVC(kernel='poly', degree=2, C=50).fit(X, y)
+plot_decision_regions(X=X, 
+                      y=y,
+                      clf=poly_svc, 
+                      legend=1)
+plt.show()
